a29b679e05a2387e5fcaeb11352f7c425356c27f
maher <gomemahero@gmail.com>
2022-01-15 19:46:43 +0300

    loop posts fromddatabase

--- include/header.php
+++ include/header.php
@@ -17,6 +17,12 @@
     <!-- Custom CSS -->
     <link href="css/blog-home.css" rel="stylesheet">
 
+
+
+
+
+
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
