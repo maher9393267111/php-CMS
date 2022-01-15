cc4d491578d1694b958a21fa78e7a84ead8769da
maher <gomemahero@gmail.com>
2022-01-15 22:43:09 +0300

    search with post_tag fild

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,3 +1,55 @@
+<?php   include 'db.php'  ?>
+
+<?php
+
+if(isset($_POST['submit'])){
+
+
+
+
+ $search =$_POST['search'];
+
+
+$query = "SELECT * FROM post WHERE post_tag LIKE  '%$search %' ";
+
+$search_query =   mysqli_query($conn,$query) ;
+
+if(!$search_query){
+
+
+die('query failed' . mysqli_error($conn));
+
+
+}
+
+
+
+
+
+$rowscount = mysqli_num_rows($search_query);
+
+
+if($rowscount == 0){
+
+
+echo  'NO Result sorry';
+
+}
+
+
+if($rowscount  > 0){
+
+
+    echo $rowscount ;
+
+    }
+
+
+}
+
+
+
+?>
 
 
  <div class="col-md-4">
@@ -5,15 +57,27 @@
 <!-- Blog Search Well -->
 <div class="well">
     <h4>Blog Search</h4>
+
+   <!-- search form -->
+
+<form action="search.php" method='post'>
+
     <div class="input-group">
-        <input type="text" class="form-control">
+        <input type="text" name= 'search' class="form-control">
         <span class="input-group-btn">
-            <button class="btn btn-default" type="button">
-                <span class="glyphicon glyphicon-search"></span>
+            <button class="btn btn-default" name='submit' type="submit" >
+                <span class="glyphicon glyphicon-search">search</span>
         </button>
         </span>
     </div>
     <!-- /.input-group -->
+
+
+    </form>
+
+       <!-- /.search form -->
+
+
 </div>
 
 <!-- Blog Categories Well -->
